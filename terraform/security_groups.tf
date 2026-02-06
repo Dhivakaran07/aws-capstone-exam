390aba86f06ad355e59a159d970651b588a0655d
Dhivakaran07 <dhivamoorthy735@gmail.com>
2026-02-06 07:43:35 +0000

    Update security_groups.tf

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -2,34 +2,25 @@ resource "aws_security_group" "web_sg" {
   vpc_id = aws_vpc.streamline.id
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["YOUR_IP/32"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_cidr]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
-resource "aws_security_group" "db_sg" {
-  vpc_id = aws_vpc.streamline.id
-
-  ingress {
-    from_port = 3306
-    to_port = 3306
-    protocol = "tcp"
-    security_groups = [aws_security_group.web_sg.id]
-  }
+  tags = { Name = "streamline-web-sg" }
 }

61f39220cf41bd2a71dd839185853b20ea6cbf9a
Dhivakaran07 <dhivamoorthy735@gmail.com>
2026-02-06 07:43:12 +0000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,5 @@
-variable "key_name" {
-  default ="streamline-key"
+variable "ssh_cidr" {
+  description = "Your public IP in CIDR notation (example: 1.2.3.4/32)"
+  type        = string
+  default     = "0.0.0.0/0"
 }

7761c561355f2f879affe381c669e64ab7692a64
Dhivakaran07 <dhivamoorthy735@gmail.com>
2026-02-06 07:36:48 +0000

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,26 +1,32 @@
 resource "aws_instance" "web1" {
-  ami = "ami-0b6c6ebed2801a5cb"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.public1.id
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public1.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
-  key_name = var.key_name
+  key_name               = var.key_name
+
+  tags = { Name = "streamline-web1" }
 }
+
 resource "aws_instance" "web2" {
-  ami = "ami-0b6c6ebed2801a5cb"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.public2.id
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public2.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
-  key_name = var.key_name
+  key_name               = var.key_name
+
+  tags = { Name = "streamline-web2" }
 }
-terraform/alb.tf
-resource "aws_lb" "alb" {
-  name = "streamline-alb"
-  load_balancer_type = "application"
-  subnets = [aws_subnet.public1.id, aws_subnet.public2.id]
-  security_groups = [aws_security_group.web_sg.id]
+
+# Attach EC2s to the ALB target group
+resource "aws_lb_target_group_attachment" "web1_attach" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.web1.id
+  port             = 80
 }
-resource "aws_lb_target_group" "tg" {
-  port = 80
-  protocol = "HTTP"
-  vpc_id = aws_vpc.streamline.id
+
+resource "aws_lb_target_group_attachment" "web2_attach" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.web2.id
+  port             = 80
 }

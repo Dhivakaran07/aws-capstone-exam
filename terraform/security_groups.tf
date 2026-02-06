d8be64c8489b8280b8d526170461bd993a2688bc
Dhivakaran07 <dhivamoorthy735@gmail.com>
2026-02-06 07:51:42 +0000

    Update security_groups.tf

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -24,3 +24,24 @@ resource "aws_security_group" "web_sg" {
 
   tags = { Name = "streamline-web-sg" }
 }
+
+resource "aws_security_group" "db_sg" {
+  vpc_id = aws_vpc.streamline.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "streamline-db-sg" }
+}
+``
